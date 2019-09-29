27ccf172d5a632c86be31d9fcd44c2714d50ceca
marcelo <marc29dias@yahoo.com.br>
2019-09-29 12:13:02 -0300

    Inclusao de autenticacao

---
+++ AuthenticationApi/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationApi.Models;
+using AuthenticationApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userService"></param>
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] Login loginParam)
+        {
+            var token = _userService.Authenticate(loginParam.Username, loginParam.Password);
+
+            if (token == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(token);
+        }
+    }
+}
